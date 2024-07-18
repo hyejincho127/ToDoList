3735f0c8e08e3270f47c5ff67a6975db01b19d88
Hyejin Cho <hyejinjohyejin@gmail.com>
2024-07-18 14:16:39 +0000

    modi

--- api/json-server.js
+++ api/json-server.js
@@ -2,10 +2,11 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../src/data/tasks.json'));
+const router = jsonServer.router(path.join(__dirname, '/src/data/tasks.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
 module.exports = server;
+
